4d3b5ca4cd4849fabcdaa223751e18ed86692bd9
Xiao Yi <369713644@qq.com>
2018-04-05 20:15:02 -0400

    Add precipitation data

--- src/data_clean_integrate.py
+++ src/data_clean_integrate.py
@@ -135,12 +135,159 @@ def temperature_data_pre_process():
     # print(files)
 
 
+def output_precipitation_production_csv(state, county, station):
+    """
+
+    :param state:
+    :param county:
+    :param station:
+    :return:
+    """
+    headers = ['state', 'county', 'station', 'year',
+               'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
+               'plant_acres', 'harvest_acres', 'yield_acres', 'bales']
+
+    result = {
+        'state': '',
+        'county': '',
+        'station': '',
+        'year': '',
+        'jan': '',
+        'feb': '',
+        'mar': '',
+        'apr': '',
+        'may': '',
+        'jun': '',
+        'jul': '',
+        'aug': '',
+        'sep': '',
+        'oct': '',
+        'nov': '',
+        'dec': '',
+        'plant_acres': '',
+        'harvest_acres': '',
+        'yield_acres': '',
+        'bales': ''
+    }
+
+    production_path = '../data_sets/cotton_production_data/' + state + '-' + county + '.xla'
+    with open(production_path, 'r') as p_f:
+        p_lines = p_f.readlines()
+        for p_line in p_lines:
+            tmp_list = p_line.split('\t')
+
+            result['county'] = county
+            result['state'] = state
+            result['station'] = station
+
+            if tmp_list[0].isdigit():
+                result['year'] = tmp_list[0]
+                result['plant_acres'] = tmp_list[1]
+                result['harvest_acres'] = tmp_list[2]
+                result['yield_acres'] = tmp_list[3]
+                result['bales'] = tmp_list[4]
+
+            precipitation_path = '../data_sets/precipitation_data/ushcn.v2.5.5.20180319/' + station + '.raw.prcp'
+            with open(precipitation_path, 'r') as t_f:
+                t_lines = t_f.readlines()
+                for t_line in t_lines:
+                    tmp_list = t_line.split(' ')[:-1]
+
+                    while '' in tmp_list:
+                        tmp_list.remove('')
+                    while 'X' in tmp_list:
+                        tmp_list.remove('X')
+                    # print(tmp_list)
+
+                    year = tmp_list[1]
+                    if year == result['year']:
+                        result['jan'] = re.findall('\-\d+|\d+', tmp_list[2])[0]
+                        result['feb'] = re.findall('\-\d+|\d+', tmp_list[3])[0]
+                        result['mar'] = re.findall('\-\d+|\d+', tmp_list[4])[0]
+                        result['apr'] = re.findall('\-\d+|\d+', tmp_list[5])[0]
+                        result['may'] = re.findall('\-\d+|\d+', tmp_list[6])[0]
+                        result['jun'] = re.findall('\-\d+|\d+', tmp_list[7])[0]
+                        result['jul'] = re.findall('\-\d+|\d+', tmp_list[8])[0]
+                        result['aug'] = re.findall('\-\d+|\d+', tmp_list[9])[0]
+                        result['sep'] = re.findall('\-\d+|\d+', tmp_list[10])[0]
+                        result['oct'] = re.findall('\-\d+|\d+', tmp_list[11])[0]
+                        result['nov'] = re.findall('\-\d+|\d+', tmp_list[12])[0]
+                        result['dec'] = re.findall('\-\d+|\d+', tmp_list[13])[0]
+
+                        print(result)
+                        with open('./precipitation_production.csv', 'a+', newline='') as output_f:
+                            writer = csv.DictWriter(output_f, headers)
+                            writer.writerow(result)
+
+    return None
+
+
+def precipitation_data_pre_process():
+    """
+
+    :return:
+    """
+    df = pd.read_csv('../data_sets/precipitation_data/ushcn-v2.5-stations.csv')
+    # print(df)
+
+    headers = ['state', 'county', 'station', 'year',
+               'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
+               'plant_acres', 'harvest_acres', 'yield_acres', 'bales']
+    with open('./precipitation_production.csv', 'w') as output_f:
+        writer = csv.DictWriter(output_f, headers)
+        writer.writeheader()
+
+    files = os.listdir('../data_sets/cotton_production_data/')
+    for file in files:
+        try:
+            c_s = file.split('.')[0].split('-')
+            state = c_s[0]
+            county = c_s[1]
+            station_list = df[(df.county == county) & (df.state == state)].station.tolist()
+            # print(station_list)
+
+            if len(station_list) != 0:
+                for station in station_list:
+
+                    precipitation_file_path = '../data_sets/precipitation_data/ushcn.v2.5.5.20180319/' + station + '.raw.prcp'
+                    try:
+                        with open(precipitation_file_path, 'r') as f:
+                            output_precipitation_production_csv(state, county, station)
+                            break
+
+                    except FileNotFoundError as e:
+                        # print(temperature_file_path)
+                        continue
+
+        except IndexError as e:
+            continue
+            # print(e)
+
+    # print(files)
+
+
+def merge_precipitation_temperature_production_data():
+    """
+
+    :return:
+    """
+    p_df = pd.read_csv('../data_sets/precipitation_production.csv')
+    t_df = pd.read_csv('../data_sets/temperature_production.csv')
+
+    df = pd.merge(p_df, t_df, how='inner',
+                  on=['state', 'county', 'year', 'plant_acres', 'harvest_acres', 'yield_acres', 'bales'])
+
+    df.to_csv('../data_sets/precipitation_temperature_production.csv', index=None)
+
+
 def main():
     """
 
     :return:
     """
-    temperature_data_pre_process()
+    # temperature_data_pre_process()
+    # precipitation_data_pre_process()
+    merge_precipitation_temperature_production_data()
 
 
 main()
--- src/data_regress.py
+++ src/data_regress.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LassoCV
+from sklearn.feature_selection import SelectFromModel
 import matplotlib.pyplot as plt
 
 
@@ -21,25 +23,27 @@ def data_pre_process(file_path):
     df['last_yield_acres'] = np.nan
     df['last_bales'] = np.nan
 
-    df = df[['state', 'county', 'station', 'year',
-             'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
-             'last_plant_acres', 'last_harvest_acres', 'last_yield_acres', 'last_bales',
-             'plant_acres', 'harvest_acres', 'yield_acres', 'bales']]
+    df = df[
+        ['state', 'county', 'station_x', 'year', 'jan_x', 'feb_x', 'mar_x', 'apr_x', 'may_x', 'jun_x', 'jul_x', 'aug_x',
+         'sep_x', 'oct_x', 'nov_x', 'dec_x', 'station_y', 'jan_y', 'feb_y', 'mar_y', 'apr_y', 'may_y', 'jun_y', 'jul_y',
+         'aug_y', 'sep_y', 'oct_y', 'nov_y', 'dec_y', 'last_plant_acres', 'last_harvest_acres', 'last_yield_acres',
+         'last_bales', 'plant_acres', 'harvest_acres', 'yield_acres', 'bales']]
 
     for i in range(df.shape[0]):
         if i > 0:
-            if df.iloc[i - 1, 0] == df.iloc[i, 0] and df.iloc[i - 1, 1] == df.iloc[i, 1] and df.iloc[i - 1, 3] == \
-                    df.iloc[i, 3] - 1:
-                df.iloc[i, 16] = df.iloc[i - 1, 20]
-                df.iloc[i, 17] = df.iloc[i - 1, 21]
-                df.iloc[i, 18] = df.iloc[i - 1, 22]
-                df.iloc[i, 19] = df.iloc[i - 1, 23]
+            if df.iloc[i - 1, 0] == df.iloc[i, 0] and df.iloc[i - 1, 1] == df.iloc[i, 1] and df.iloc[i - 1, 3] - 1 == \
+                    df.iloc[i, 3]:
+                df.iloc[i, -8] = df.iloc[i - 1, -4]
+                df.iloc[i, -7] = df.iloc[i - 1, -3]
+                df.iloc[i, -6] = df.iloc[i - 1, -2]
+                df.iloc[i, -5] = df.iloc[i - 1, -1]
 
     df = df.dropna()
 
-    df = df[['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
-             'last_plant_acres', 'last_harvest_acres', 'last_yield_acres', 'last_bales',
-             'plant_acres', 'harvest_acres', 'yield_acres', 'bales']]
+    df = df[['jan_x', 'feb_x', 'mar_x', 'apr_x', 'may_x', 'jun_x', 'jul_x', 'aug_x', 'sep_x', 'oct_x', 'nov_x', 'dec_x',
+             'jan_y', 'feb_y', 'mar_y', 'apr_y', 'may_y', 'jun_y', 'jul_y', 'aug_y', 'sep_y', 'oct_y', 'nov_y', 'dec_y',
+             'last_plant_acres', 'last_harvest_acres', 'last_yield_acres', 'last_bales', 'plant_acres', 'harvest_acres',
+             'yield_acres', 'bales']]
 
     df = df.sample(frac=1.0)
     cut_idx = int(round(0.1 * df.shape[0]))
@@ -60,17 +64,24 @@ def data_regression(train_data_set, test_data_set):
     :param test_data_set:
     :return:
     """
-    train_x = train_data_set[:, :16]
-    train_y_1 = train_data_set[:, 16]
-    train_y_2 = train_data_set[:, 17]
-    train_y_3 = train_data_set[:, 18]
-    train_y_4 = train_data_set[:, 19]
+    train_x = train_data_set[:, :27]
+    # train_y_1 = train_data_set[:, 16]
+    # train_y_2 = train_data_set[:, 17]
+    train_y_3 = train_data_set[:, 30]
+    # train_y_4 = train_data_set[:, 19]
 
-    test_x = test_data_set[:, :16]
-    test_y_1 = test_data_set[:, 16]
-    test_y_2 = test_data_set[:, 17]
-    test_y_3 = test_data_set[:, 18]
-    test_y_4 = test_data_set[:, 19]
+    test_x = test_data_set[:, :27]
+    # test_y_1 = test_data_set[:, 16]
+    # test_y_2 = test_data_set[:, 17]
+    test_y_3 = test_data_set[:, 30]
+    # test_y_4 = test_data_set[:, 19]
+
+    # feature selection
+    clf = LassoCV()
+    sfm = SelectFromModel(clf)
+    train_x = sfm.fit_transform(train_x, train_y_3)
+    test_x = sfm.transform(test_x)
+    # print(test_x.shape)
 
     # Neural Network Regression
     regress_neural = MLPRegressor().fit(train_x, train_y_3)
@@ -110,7 +121,7 @@ def main():
 
     :return:
     """
-    filename = '../data_sets/temperature_production.csv'
+    filename = '../data_sets/precipitation_temperature_production.csv'
     train_data_set, test_data_set = data_pre_process(filename)
     data_regression(train_data_set, test_data_set)
 
