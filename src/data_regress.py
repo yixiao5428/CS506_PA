3b3f47e23dcdee43ea5a9944455507924c32f11a
Xiao Yi <369713644@qq.com>
2018-04-11 14:58:03 -0400

    Add disease data

--- src/data_regress.py
+++ src/data_regress.py
@@ -26,24 +26,29 @@ def data_pre_process(file_path):
     df = df[
         ['state', 'county', 'station_x', 'year', 'jan_x', 'feb_x', 'mar_x', 'apr_x', 'may_x', 'jun_x', 'jul_x', 'aug_x',
          'sep_x', 'oct_x', 'nov_x', 'dec_x', 'station_y', 'jan_y', 'feb_y', 'mar_y', 'apr_y', 'may_y', 'jun_y', 'jul_y',
-         'aug_y', 'sep_y', 'oct_y', 'nov_y', 'dec_y', 'last_plant_acres', 'last_harvest_acres', 'last_yield_acres',
-         'last_bales', 'plant_acres', 'harvest_acres', 'yield_acres', 'bales']]
+         'aug_y', 'sep_y', 'oct_y', 'nov_y', 'dec_y', 'total_percent_lost', 'last_plant_acres', 'last_harvest_acres',
+         'last_yield_acres', 'last_bales', 'plant_acres', 'harvest_acres', 'yield_acres', 'bales']]
+
+    df = df.sort_values(by=['state', 'county', 'year'])
 
     for i in range(df.shape[0]):
         if i > 0:
-            if df.iloc[i - 1, 0] == df.iloc[i, 0] and df.iloc[i - 1, 1] == df.iloc[i, 1] and df.iloc[i - 1, 3] - 1 == \
-                    df.iloc[i, 3]:
+            if df.iloc[i - 1, 0] == df.iloc[i, 0] and df.iloc[i - 1, 1] == df.iloc[i, 1] and df.iloc[i - 1, 3] == \
+                    df.iloc[i, 3] - 1:
                 df.iloc[i, -8] = df.iloc[i - 1, -4]
                 df.iloc[i, -7] = df.iloc[i - 1, -3]
                 df.iloc[i, -6] = df.iloc[i - 1, -2]
                 df.iloc[i, -5] = df.iloc[i - 1, -1]
 
+    # df.to_csv('./temp.csv')
     df = df.dropna()
+    # print(df)
 
     df = df[['jan_x', 'feb_x', 'mar_x', 'apr_x', 'may_x', 'jun_x', 'jul_x', 'aug_x', 'sep_x', 'oct_x', 'nov_x', 'dec_x',
              'jan_y', 'feb_y', 'mar_y', 'apr_y', 'may_y', 'jun_y', 'jul_y', 'aug_y', 'sep_y', 'oct_y', 'nov_y', 'dec_y',
-             'last_plant_acres', 'last_harvest_acres', 'last_yield_acres', 'last_bales', 'plant_acres', 'harvest_acres',
-             'yield_acres', 'bales']]
+             'total_percent_lost',
+             'last_plant_acres', 'last_harvest_acres', 'last_yield_acres', 'last_bales',
+             'plant_acres', 'harvest_acres', 'yield_acres', 'bales']]
 
     df = df.sample(frac=1.0)
     cut_idx = int(round(0.1 * df.shape[0]))
@@ -64,16 +69,16 @@ def data_regression(train_data_set, test_data_set):
     :param test_data_set:
     :return:
     """
-    train_x = train_data_set[:, :27]
+    train_x = train_data_set[:, :28]
     # train_y_1 = train_data_set[:, 16]
     # train_y_2 = train_data_set[:, 17]
-    train_y_3 = train_data_set[:, 30]
+    train_y_3 = train_data_set[:, 31]
     # train_y_4 = train_data_set[:, 19]
 
-    test_x = test_data_set[:, :27]
+    test_x = test_data_set[:, :28]
     # test_y_1 = test_data_set[:, 16]
     # test_y_2 = test_data_set[:, 17]
-    test_y_3 = test_data_set[:, 30]
+    test_y_3 = test_data_set[:, 31]
     # test_y_4 = test_data_set[:, 19]
 
     # feature selection
@@ -121,7 +126,7 @@ def main():
 
     :return:
     """
-    filename = '../data_sets/precipitation_temperature_production.csv'
+    filename = '../data_sets/disease_precipitation_temperature_production.csv'
     train_data_set, test_data_set = data_pre_process(filename)
     data_regression(train_data_set, test_data_set)
 
